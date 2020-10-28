2eda7fc6ccae240c647250aad81cf412aeab015c
Brad Sickles <brad.sickles@gmail.com>
2020-10-28 14:25:44 -0400

    Developed ns_workspace

--- ns/provider_test.go
+++ ns/provider_test.go
@@ -1,10 +1,15 @@
 package ns
 
 import (
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
+
+var testAccProviders = map[string]terraform.ResourceProvider{
+	"ns": Provider().(*schema.Provider),
+}
 
 func TestProvider(t *testing.T) {
 	t.Run("runs internal validation without error", func(t *testing.T) {
@@ -12,4 +17,8 @@ func TestProvider(t *testing.T) {
 			t.Fatalf("err: %s", err)
 		}
 	})
+}
+
+func testAccPreCheck(t *testing.T) {
+	// NOTE: Add prechecks for all tests here
 }
