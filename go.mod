6c00b1eb3531ccb4b325188f1e0565c5bad1ff92
Brad Sickles <brad.sickles@gmail.com>
2020-10-28 11:46:28 -0400

    Initial scaffolding of provider

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nullstone-io/terraform-provider-ns
+
+go 1.15
+
+require github.com/hashicorp/terraform-plugin-sdk v1.16.0
---
+++ ns/provider.go
@@ -0,0 +1,14 @@
+package ns
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func Provider() terraform.ResourceProvider {
+	p := &schema.Provider{
+		Schema: map[string]*schema.Schema{
+		},
+	}
+	return p
+}
