e9092f2eb4807973fcaa257cd92fe5bf83a718aa
Brad Sickles <brad.sickles@gmail.com>
2021-04-17 00:30:11 -0400

    Still allow TFE_TOKEN, TFE_ADDRESS

--- ns/tfe_config.go
+++ ns/tfe_config.go
@@ -9,13 +9,19 @@ import (
 
 func NewTfeConfig() *tfe.Config {
 	cfg := tfe.DefaultConfig()
-	cfg.Address = api.DefaultAddress
-	if val := os.Getenv(api.AddressEnvVar); val != "" {
-		cfg.Address = val
+	// If TFE_ADDRESS is missing, we will look at NULLSTONE_ADDR, then use the default address
+	if cfg.Address == "" {
+		cfg.Address = api.DefaultAddress
+		if val := os.Getenv(api.AddressEnvVar); val != "" {
+			cfg.Address = val
+		}
 	}
 	cfg.BasePath = "/terraform/v2/"
-	if val := os.Getenv(api.ApiKeyEnvVar); val != "" {
-		cfg.Token = val
+	// If TFE_TOKEN is missing, we will look at NULLSTONE_API_KEY
+	if cfg.Token == "" {
+		if val := os.Getenv(api.ApiKeyEnvVar); val != "" {
+			cfg.Token = val
+		}
 	}
 	return cfg
 }
