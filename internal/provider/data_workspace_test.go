357cf566b146c2870789f1fb819ad6f780ef5b8d
Brad Sickles <brad.sickles@gmail.com>
2021-05-19 14:30:51 -0400

    Update workspace tests

--- internal/provider/data_workspace_test.go
+++ internal/provider/data_workspace_test.go
@@ -14,8 +14,13 @@ func TestDataWorkspace(t *testing.T) {
 		resource.TestCheckResourceAttr("data.ns_workspace.this", `tags.Stack`, "stack0"),
 		resource.TestCheckResourceAttr("data.ns_workspace.this", `tags.Env`, "env0"),
 		resource.TestCheckResourceAttr("data.ns_workspace.this", `tags.Block`, "block0"),
-		resource.TestCheckResourceAttr("data.ns_workspace.this", "hyphenated_name", "stack0-env0-block0"),
-		resource.TestCheckResourceAttr("data.ns_workspace.this", "slashed_name", "stack0/env0/block0"),
+		resource.TestCheckResourceAttr("data.ns_workspace.this", "stack_id", "100"),
+		resource.TestCheckResourceAttr("data.ns_workspace.this", "stack_name", "stack0"),
+		resource.TestCheckResourceAttr("data.ns_workspace.this", "block_id", "101"),
+		resource.TestCheckResourceAttr("data.ns_workspace.this", "block_name", "block0"),
+		resource.TestCheckResourceAttr("data.ns_workspace.this", "block_ref", "yellow-giraffe"),
+		resource.TestCheckResourceAttr("data.ns_workspace.this", "env_id", "102"),
+		resource.TestCheckResourceAttr("data.ns_workspace.this", "env_name", "env0"),
 	)
 
 	t.Run("sets up attributes properly hard-coded", func(t *testing.T) {
@@ -24,9 +29,13 @@ provider "ns" {
   organization = "org0"
 }
 data "ns_workspace" "this" {
-  stack = "stack0"
-  env   = "env0"
-  block = "block0"
+  stack_id   = "100"
+  stack_name = "stack0"
+  block_id   = "101"
+  block_name = "block0"
+  block_ref  = "yellow-giraffe"
+  env_id     = "102"
+  env_name   = "env0"
 }
 `)
 		getNsConfig, _ := mockNs(nil)
@@ -53,9 +62,13 @@ data "ns_workspace" "this" {}
 		getNsConfig, _ := mockNs(nil)
 		getTfeConfig, _ := mockTfe(nil)
 
-		os.Setenv("NULLSTONE_STACK", "stack0")
-		os.Setenv("NULLSTONE_ENV", "env0")
-		os.Setenv("NULLSTONE_BLOCK", "block0")
+		os.Setenv("NULLSTONE_STACK_ID", "100")
+		os.Setenv("NULLSTONE_STACK_NAME", "stack0")
+		os.Setenv("NULLSTONE_BLOCK_ID", "101")
+		os.Setenv("NULLSTONE_BLOCK_NAME", "block0")
+		os.Setenv("NULLSTONE_BLOCK_REF", "yellow-giraffe")
+		os.Setenv("NULLSTONE_ENV_ID", "102")
+		os.Setenv("NULLSTONE_ENV_NAME", "env0")
 
 		resource.UnitTest(t, resource.TestCase{
 			ProtoV5ProviderFactories: protoV5ProviderFactories(getNsConfig, getTfeConfig),
