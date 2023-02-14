3deabd47afa2eb267d2877a803ce97b216502ecf
Scott Sickles <scottsickles@gmail.com>
2023-02-13 19:47:13 -0500

    added debug logging so we can determine what is going on

--- internal/provider/data_env_variables.go
+++ internal/provider/data_env_variables.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-go/tftypes"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"regexp"
 )
 
@@ -110,6 +111,9 @@ func (d *dataEnvVariables) Read(ctx context.Context, config map[string]tftypes.V
 	inputEnvVariables := extractMapFromConfig(config, "input_env_variables")
 	inputSecrets := extractMapFromConfig(config, "input_secrets")
 
+	tflog.Debug(ctx, "input_env_variables", inputEnvVariables)
+	tflog.Debug(ctx, "input_secrets", inputSecrets)
+
 	// make sure we copy these so our changes below don't affect the original values
 	envVariables := copyMap(inputEnvVariables)
 	secrets := copyMap(inputSecrets)
@@ -125,6 +129,7 @@ func (d *dataEnvVariables) Read(ctx context.Context, config map[string]tftypes.V
 			result := regex.ReplaceAllString(extractStringFromTfValue(v), extractStringFromTfValue(secret))
 			// if a match was found and replaced, this env variable is now a secret
 			if result != extractStringFromTfValue(v) {
+				tflog.Debug(ctx, fmt.Sprintf("Found and replaced secret in env variable: %s", k), result)
 				delete(envVariables, k)
 				secrets[k] = tftypes.NewValue(tftypes.String, result)
 			}
@@ -134,6 +139,9 @@ func (d *dataEnvVariables) Read(ctx context.Context, config map[string]tftypes.V
 			// we don't want to replace the secret with itself (this will prevent an infinite loop)
 			if k != key {
 				result := regex.ReplaceAllString(extractStringFromTfValue(v), extractStringFromTfValue(secret))
+				if result != extractStringFromTfValue(v) {
+					tflog.Debug(ctx, fmt.Sprintf("Found and replaced secret (%s) in secret: %s", k), result)
+				}
 				secrets[k] = tftypes.NewValue(tftypes.String, result)
 			}
 		}
@@ -147,11 +155,17 @@ func (d *dataEnvVariables) Read(ctx context.Context, config map[string]tftypes.V
 			// we don't want to replace the env variable with itself (this will prevent an infinite loop)
 			if k != key {
 				result := regex.ReplaceAllString(extractStringFromTfValue(v), extractStringFromTfValue(value))
+				if result != extractStringFromTfValue(v) {
+					tflog.Debug(ctx, fmt.Sprintf("Found and replaced env variable (%s) in env variable: %s", k), result)
+				}
 				envVariables[k] = tftypes.NewValue(tftypes.String, result)
 			}
 		}
 		for k, v := range secrets {
 			result := regex.ReplaceAllString(extractStringFromTfValue(v), extractStringFromTfValue(value))
+			if result != extractStringFromTfValue(v) {
+				tflog.Debug(ctx, fmt.Sprintf("Found and replaced env variable (%s) in secret: %s", k), result)
+			}
 			secrets[k] = tftypes.NewValue(tftypes.String, result)
 		}
 	}
@@ -168,6 +182,12 @@ func (d *dataEnvVariables) Read(ctx context.Context, config map[string]tftypes.V
 
 	// calculate the unique id for this data source based on a hash of the resulting env variables and secrets
 	id := d.HashFromValues(envVariables, secrets)
+
+	tflog.Debug(ctx, "id", id)
+	tflog.Debug(ctx, "env_variable_keys", envVariableKeys)
+	tflog.Debug(ctx, "secret_keys", secretKeys)
+	tflog.Debug(ctx, "env_variables", envVariables)
+	tflog.Debug(ctx, "secrets", secrets)
 
 	return map[string]tftypes.Value{
 		"id":                  tftypes.NewValue(tftypes.String, id),
